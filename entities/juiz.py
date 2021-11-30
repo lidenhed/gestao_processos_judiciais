f28ca1372ab3153df24ab35fe4672f6920b8b2cd
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-11-30 19:46:07 -0300

    Retirei os atributos logado e senha de juiz e parte pois já estão declarados na superclasse 'Usuario'

--- entities/juiz.py
+++ entities/juiz.py
@@ -4,23 +4,10 @@ from entities.usuario import Usuario
 class Juiz(Usuario):
 
     def __init__(self, nome: str, cpf: str, matricula: int, senha: str, logado: bool = False):
-        super().__init__(nome, cpf, senha)
-        self.__senha = senha
-        self.__logado = logado
+        super().__init__(nome, cpf, senha, logado)
+
         self.__matricula = matricula
 
-    @property
-    def senha(self):
-        return self.__senha
-
-    @property
-    def logado(self):
-        return self.__logado
-
-    @logado.setter
-    def logado(self, logado: bool):
-        self.__logado = logado
-        
     @property
     def matricula(self):
         return self.__matricula
--- entities/parte.py
+++ entities/parte.py
@@ -4,22 +4,8 @@ from entities.usuario import Usuario
 class Parte(Usuario):
 
     def __init__(self, nome: str, cpf: str, senha: str, advogado: str, logado: bool = False):
-        super().__init__(nome, cpf, senha)
-        self.__senha = senha
+        super().__init__(nome, cpf, senha, logado)
         self.__advogado = advogado
-        self.__logado = logado
-
-    @property
-    def senha(self):
-        return self.__senha
-
-    @property
-    def logado(self):
-        return self.__logado
-
-    @logado.setter
-    def logado(self, logado: bool):
-        self.__logado = logado
 
     @property
     def advogado(self):

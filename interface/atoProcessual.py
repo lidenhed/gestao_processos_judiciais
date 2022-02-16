b844f33f145b59ce6bfc35c79cdb2942a95b5b82
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-02-16 10:33:24 -0300

    Adicionado Interface atoProcessual

---
+++ interface/atoProcessual.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as psg
+
+class InterfaceAtoProcessual:
+
+    def __init__(self, controlador):
+        self.__controlador = controlador
+        self.__window = None
+            
+    def tela_realizar_ato(self):
+        while True:
+            layout_ato_processual = [
+                [psg.Text('Preencha os campos abaixo:', size=(30, 1))],
+                [psg.Text('', size=(30, 1))],
+                [psg.Checkbox('Solicitar urgência:     ',"CHECKBOX", size=(30, 1))],
+                [psg.Text('Anexe aqui seu arquivo:', size=(30, 1)), psg.Button('Anexar')],
+                [psg.Button('Enviar'), psg.Button('Voltar')]
+            ]
+            tela_realizar_ato = psg.Window('Tela Realizar Ato').Layout(layout_ato_processual)
+            event, values = tela_realizar_ato.Read()
+            tela_realizar_ato.Close()
+            if event == 'Voltar' or event == psg.WIN_CLOSED:
+                break
+            else:
+                return values
+                
+    def aviso(self, msg):
+        layout_aviso = [
+            [psg.Text(msg)],
+            [psg.Ok()]
+        ]
+        tela_aviso = psg.Window('Aviso').Layout(layout_aviso)
+        tela_aviso.Read()
+        tela_aviso.Close()
--- interface/processo.py
+++ interface/processo.py
@@ -26,23 +26,6 @@ class InterfaceProcesso:
                 break
             else:
                 return values
-            
-    def tela_realizar_ato(self):
-        while True:
-            layout_ato_processual = [
-                [psg.Text('Preencha os campos abaixo:', size=(30, 1))],
-                [psg.Text('', size=(30, 1))],
-                [psg.Checkbox('Solicitar urgência:     ',"CHECKBOX", size=(30, 1))],
-                [psg.Text('Anexe aqui seu arquivo:', size=(30, 1)), psg.Button('Anexar')],
-                [psg.Button('Enviar'), psg.Button('Voltar')]
-            ]
-            tela_realizar_ato = psg.Window('Tela Realizar Ato').Layout(layout_ato_processual)
-            event, values = tela_realizar_ato.Read()
-            tela_realizar_ato.Close()
-            if event == 'Voltar' or event == psg.WIN_CLOSED:
-                break
-            else:
-                return values
                 
     def aviso(self, msg):
         layout_aviso = [

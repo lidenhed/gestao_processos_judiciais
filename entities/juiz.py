bfdc08e950d601acd8569a3aff292500bbaa4b4d
lidenhed <61945472+lidenhed@users.noreply.github.com>
2022-02-16 15:12:32 -0300

    Update juiz.py
    Professora orientou que fosse tirado o atributo "logado".

--- entities/juiz.py
+++ entities/juiz.py
@@ -3,7 +3,7 @@ from entities.usuario import Usuario
 
 class Juiz(Usuario):
 
-    def __init__(self, nome: str, login: str, matricula: int, senha: str, logado: bool = False):
+    def __init__(self, nome: str, login: str, matricula: int, senha: str):
         super().__init__(nome, login, senha, logado)
         self.__matricula = matricula
 
